7f06f5711a12a562f54d459bf20411239b0a0de8
Cong <cong34dk@gmail.com>
2023-11-22 01:38:10 +0700

    update UI Admin

--- fptshop/angular/QLSanPham.js
+++ fptshop/angular/QLSanPham.js
@@ -1,8 +1,9 @@
 var app = angular.module('AppBanDienThoai', []);
 
     app.controller('ProductController', function ($scope, $http) {
-        $scope.products = [];
+        $scope.products = []; // Dữ liệu sản phẩm được lấy từ API
 
+        // Hàm lấy danh sách sản phẩm
         $scope.LoadSanPham = function (){
             $http({
                 method: "GET",
@@ -13,6 +14,23 @@ var app = angular.module('AppBanDienThoai', []);
                 console.log('Có lỗi khi lấy dữ liệu từ API', error);
             });
         }
+
+        // Hàm xóa sản phẩm
+        $scope.deleteProduct = function(productId) {
+            if (confirm('Bạn có chắc chắn muốn xóa sản phẩm này không?')) {
+                $http({
+                    method: "DELETE",
+                    url: `https://localhost:44388/api/SanPham/delete/${productId}`
+                }).then(function (response) {
+                    alert(response.data.message);
+                    // Sau khi xóa thành công, cập nhật lại danh sách sản phẩm
+                    $scope.LoadSanPham();
+                }).catch(function (error) {
+                    alert('Có lỗi khi xóa sản phẩm');
+                    console.log('Lỗi khi xóa sản phẩm', error);
+                });
+            }
+        };
 
           // Gọi API khi trang được tải
           $scope.LoadSanPham()
