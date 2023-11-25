445351db4cc1d7e96718343a72552ce6f622c1f6
Cong <cong34dk@gmail.com>
2023-11-26 00:44:12 +0700

    update function admin oki

--- fptshop/angular/QLSanPham.js
+++ fptshop/angular/QLSanPham.js
@@ -53,6 +53,7 @@ var app = angular.module('AppBanDienThoai', []);
                     // Xử lý kết quả sau khi thêm sản phẩm thành công
                     console.log('Sản phẩm đã được thêm:', response.data);
                     // Đặt logic xử lý khi thêm sản phẩm thành công
+                    alert('Sản phẩm đã được thêm thành công');
                     $scope.LoadSanPham();
                     $scope.newProduct = {};
                 })
@@ -60,8 +61,38 @@ var app = angular.module('AppBanDienThoai', []);
                     // Xử lý khi có lỗi trong quá trình thêm sản phẩm
                     console.error('Lỗi khi thêm sản phẩm:', error);
                     // Đặt logic xử lý khi có lỗi
+                    alert('Lỗi khi thêm sản phẩm');
                 });
         };
+
+
+        //Hàm sửa sản phẩm
+        $scope.openEditModal = function (product) {
+            $scope.isEditModalOpen = true;
+            $scope.editedProduct = angular.copy(product); // Copy thông tin sản phẩm cần sửa vào biến editedProduct
+        };
+
+        $scope.closeEditModal = function () {
+            $scope.isEditModalOpen = false;
+        };
+
+        $scope.saveEditedProduct = function () {
+            // Sử dụng $scope.editedProduct để lấy thông tin sản phẩm đã chỉnh sửa
+            // Sau khi gọi API xong, đóng modal sửa sản phẩm
+            $http.put('https://localhost:44388/api/SanPham/update-SanPham', $scope.editedProduct)
+                .then(function (response) {
+                    // Xử lý kết quả từ API (response)
+                    console.log(response.data.message); // Log message từ server
+                    alert('Sửa sản phẩm thành công')
+                    $scope.LoadSanPham()
+                    $scope.isEditModalOpen = false; // Đóng modal sau khi cập nhật thành công
+                })
+                .catch(function (error) {
+                    // Xử lý lỗi (nếu có)
+                    console.error('Lỗi khi đang sửa sản phẩm:', error);
+                });
+        };
+        
 
           // Gọi API khi trang được tải
           $scope.LoadSanPham()
