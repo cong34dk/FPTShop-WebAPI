2bd6f983d930d66620900570d73e7f94fa13d68a
Cong <cong34dk@gmail.com>
2023-11-25 16:01:35 +0700

    update function admin

--- fptshop/angular/QLSanPham.js
+++ fptshop/angular/QLSanPham.js
@@ -1,7 +1,21 @@
 var app = angular.module('AppBanDienThoai', []);
 
     app.controller('ProductController', function ($scope, $http) {
+        //Khai báo biến
         $scope.products = []; // Dữ liệu sản phẩm được lấy từ API
+
+        $scope.newProduct = {
+            "maSanPham": 0,
+            "maChuyenMuc": 31,
+            "tenSanPham": "",
+            "anhDaiDien": "",
+            "gia": null,
+            "giaGiam": null,
+            "soLuong": null,
+            "trangThai": true,
+            "luotXem": 0,
+            "dacBiet": false
+        };
 
         // Hàm lấy danh sách sản phẩm
         $scope.LoadSanPham = function (){
@@ -30,6 +44,23 @@ var app = angular.module('AppBanDienThoai', []);
                     console.log('Lỗi khi xóa sản phẩm', error);
                 });
             }
+        };
+
+        //Hàm thêm sản phẩm
+        $scope.addProduct = function() {
+            $http.post('https://localhost:44388/api/SanPham/create-SanPham', $scope.newProduct)
+                .then(function(response) {
+                    // Xử lý kết quả sau khi thêm sản phẩm thành công
+                    console.log('Sản phẩm đã được thêm:', response.data);
+                    // Đặt logic xử lý khi thêm sản phẩm thành công
+                    $scope.LoadSanPham();
+                    $scope.newProduct = {};
+                })
+                .catch(function(error) {
+                    // Xử lý khi có lỗi trong quá trình thêm sản phẩm
+                    console.error('Lỗi khi thêm sản phẩm:', error);
+                    // Đặt logic xử lý khi có lỗi
+                });
         };
 
           // Gọi API khi trang được tải
